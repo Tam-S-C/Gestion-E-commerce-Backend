bd8acb5337115ea4d950be8b27c33d1b33775c29
Tam S C <tamaracanzobre@hotmail.com>
2024-12-23 01:38:30 -0300

    extras carpeta public

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { productsRoutes } from "./routes/products.routes.js";
 import { cartsRoutes } from "./routes/carts.routes.js";
+import path from "path";
+import { __dirname } from "./dirname.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 // ROUTES
 
